2969749dc3ba6f29cee5baaa4d37e9a3a4b5e4d1
Alin Dreghiciu <adreghiciu@gmail.com>
2013-09-21 15:31:47 +0300

    Save point
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- plugins/capabilities/nexus-capabilities-plugin/src/main/resources/static/js/Nexus/capabilities/CapabilityStatusView.js
+++ plugins/capabilities/nexus-capabilities-plugin/src/main/resources/static/js/Nexus/capabilities/CapabilityStatusView.js
@@ -1,9 +1,14 @@
 /*
- * Copyright (c) 2008-2013 Sonatype, Inc.
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2013 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
  *
- * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/pro/attributions
- * Sonatype and Sonatype Nexus are trademarks of Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation.
- * M2Eclipse is a trademark of the Eclipse Foundation. All other trademarks are the property of their respective owners.
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
 /*global NX, Ext, Nexus*/
 
@@ -27,10 +32,34 @@ NX.define('Nexus.capabilities.CapabilityStatusView', {
 
         Ext.apply(self, {
             cls: 'nx-capabilities-CapabilityStatusView',
-            layout: 'border',
-            hideMode: 'offsets',
-            items: [
-            ]
+            layout : 'card',
+            region : 'center',
+            activeItem : 0,
+            bodyStyle : 'padding:0px 15px 0px 15px',
+            deferredRender : false,
+            autoScroll : false,
+            autoHeight : true,
+            border: false,
+            //frame : false,
+            //visible : false,
+            hidden: true,
+            items : {
+                xtype : 'fieldset',
+                autoHeight : true,
+                checkboxToggle : false,
+                title : 'Status',
+                anchor : Sonatype.view.FIELDSET_OFFSET,
+                collapsible : false,
+                layoutConfig : {
+                    labelSeparator : ''
+                },
+                items : {
+                    xtype : 'panel',
+                    name : 'status',
+                    layout : 'fit',
+                    border: false
+                }
+            }
         });
 
         self.constructor.superclass.initComponent.apply(self, arguments);
@@ -39,10 +68,21 @@ NX.define('Nexus.capabilities.CapabilityStatusView', {
     /**
      * Update the capability record.
      *
-     * @param repo
+     * @param capability
      */
     updateRecord: function (capability) {
-        var self = this;
+        var self = this,
+            status = self.find('name','status')[0];
+
+        if (capability.status) {
+            status.body.update(capability.status);
+            self.show();
+        }
+        else {
+            self.hide();
+            status.body.update('');
+        }
+
     }
 
 });
--- plugins/capabilities/nexus-capabilities-plugin/src/main/resources/static/js/Nexus/capabilities/CapabilityView.js
+++ plugins/capabilities/nexus-capabilities-plugin/src/main/resources/static/js/Nexus/capabilities/CapabilityView.js
@@ -1,9 +1,14 @@
 /*
- * Copyright (c) 2008-2013 Sonatype, Inc.
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2013 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
  *
- * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/pro/attributions
- * Sonatype and Sonatype Nexus are trademarks of Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation.
- * M2Eclipse is a trademark of the Eclipse Foundation. All other trademarks are the property of their respective owners.
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
 /*global NX, Ext, Nexus*/
 
@@ -21,7 +26,8 @@ NX.define('Nexus.capabilities.CapabilityView', {
 
     requires: [
         'Nexus.capabilities.Icons',
-        'Nexus.capabilities.CapabilityTypeView'
+        'Nexus.capabilities.CapabilityTypeView',
+        'Nexus.capabilities.CapabilityStatusView'
     ],
 
     /**
@@ -31,20 +37,30 @@ NX.define('Nexus.capabilities.CapabilityView', {
         var self = this;
 
         self.capabilityTypeView = NX.create('Nexus.capabilities.CapabilityTypeView');
+        self.capabilityStatusView = NX.create('Nexus.capabilities.CapabilityStatusView');
 
-        self.panel = NX.create('Ext.Panel', {
-            //layout: 'column',
-            items: self.capabilityTypeView
-        });
+        //self.panel = NX.create('Ext.Panel', {
+        //    //layout: 'column',
+        //    items: self.capabilityTypeView
+        //});
 
         Ext.apply(self, {
             cls: 'nx-capabilities-CapabilityView',
             title: 'Capability',
-            header: true,
+            //header: true,
             border: false,
             //layout: 'fit',
-
-            items: self.panel
+            //layout: {
+            //    type: 'vbox'
+            //    align : 'stretch',
+            //    pack  : 'start',
+            //},
+            //layout: 'vbox',
+            autoScroll : true,
+            items: [
+                self.capabilityTypeView,
+                self.capabilityStatusView
+            ]
         });
 
         self.constructor.superclass.initComponent.apply(self, arguments);
@@ -52,9 +68,11 @@ NX.define('Nexus.capabilities.CapabilityView', {
         // propagate 'hide' and 'deactivate' events to child views, 'show' and 'activate' handled when [re-]enabling views
         self.on('deactivate', function(panel) {
             self.capabilityTypeView.fireEvent('deactivate', self.capabilityTypeView);
+            self.capabilityStatusView.fireEvent('deactivate', self.capabilityStatusView);
         });
         self.on('hide', function(panel) {
             self.capabilityTypeView.fireEvent('hide', self.capabilityTypeView);
+            self.capabilityStatusView.fireEvent('hide', self.capabilityStatusView);
         });
 
     },
@@ -65,7 +83,9 @@ NX.define('Nexus.capabilities.CapabilityView', {
 
         self.setTitle(capability.typeName, mediator.iconFor(capability).cls);
         self.capabilityTypeView.updateRecord(capability);
-        self.capabilityTypeView.fireEvent('show', self.capabilityTypeView);
+        self.capabilityStatusView.updateRecord(capability);
+        //self.capabilityTypeView.fireEvent('show', self.capabilityTypeView);
+        //self.capabilityStatusView.fireEvent('show', self.capabilityStatusView);
     }
 
 });
